eba9cfef0c5c8932d3150067cf44ed8381153cd0
aynara-NolazcoYs <aynara.nolazco@vallegrande.edu.pe>
2026-05-15 10:19:02 -0500

    Tabla actualizada y mejorada

--- script.sql
+++ script.sql
@@ -14,21 +14,26 @@ GO
 
 CREATE TABLE Mascotas (
     id INT IDENTITY(1,1) PRIMARY KEY,
+
     nombre VARCHAR(100) NOT NULL,
-    tipo VARCHAR(50) NOT NULL,
+
+    tipo VARCHAR(20) NOT NULL,
+
     edad INT NOT NULL,
-    descripcion VARCHAR(255)
+
+    descripcion VARCHAR(255),
+
+    fecha_registro DATETIME DEFAULT GETDATE()
 );
 GO
 
 -- ============================================
--- DATOS DE PRUEBA
+-- DATO DE PRUEBA
 -- ============================================
 
-INSERT INTO Mascotas (nombre, tipo, edad, descripcion) VALUES
-('Max', 'Perro', 3, 'Muy amigable'),
-('Luna', 'Gato', 2, 'Tranquila'),
-('Rocky', 'Perro', 5, 'Protector');
+INSERT INTO Mascotas (nombre, tipo, edad, descripcion)
+VALUES
+('Max', 'Perro', 3, 'Muy amigable');
 GO
 
 -- ============================================
